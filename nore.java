ef57331838ed97f373f8871b92aecff1d4d3e90f
MhdiiFadyii <138668447+MhdiiFadyii@users.noreply.github.com>
2023-12-27 14:17:23 +0900

    Update nore.java

--- nore.java
+++ nore.java
@@ -6,6 +6,12 @@ public class Main {
     Main myObj2 = new Main();  // Object 2
     System.out.println(myObj1.x);
     System.out.println(myObj2.x);
+        Main myObj32 = new Main();  // Object 2
+    System.out.println(myObj1.x);
+    System.out.println(myObj2.x);
+        Main myObj22 = new Main();  // Object 2
+    System.out.println(myObj1.x);
+    System.out.println(myObj2.x);
   }
 }
 
